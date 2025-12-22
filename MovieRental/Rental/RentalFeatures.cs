e6b5c9663de6ae29f7c9c689de029dcd991aa53f
andrecalcada <andre_calcada424@hotmail.com>
2025-12-22 09:43:19 +0000

    finish challenge

--- MovieRental/Rental/RentalFeatures.cs
+++ MovieRental/Rental/RentalFeatures.cs
@@ -13,28 +13,47 @@ namespace MovieRental.Rental
 
 		public async Task<Rental> Save(Rental rental)
 		{
-			await _movieRentalDb.Rentals.AddAsync(rental);
-			await _movieRentalDb.SaveChangesAsync();
-			return rental;
-		}
+            await using var transaction = await _movieRentalDb.Database.BeginTransactionAsync();
+            try
+            {
+                if (rental.PaymentMethod is null)
+                    throw new InvalidOperationException("Payment method is required.");
+
+                var MbWayProvider = new PaymentProviders.MbWayProvider();
+                var PayPalProvider = new PaymentProviders.PayPalProvider();
+                var Price = rental.DaysRented * 2/*Price per day*/;
+                bool Success = false;
+                switch (rental.PaymentMethod)
+                {
+                    case "MbWay":
+                        Success = await MbWayProvider.Pay(Price);
+                        break;
+                    case "PayPal":
+                        Success = await PayPalProvider.Pay(Price);
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unsupported payment method: {rental.PaymentMethod}");
+                }
+                if(!Success)
+                    throw new InvalidOperationException("Payment failed.");
+                await _movieRentalDb.Rentals.AddAsync(rental);
+                await _movieRentalDb.SaveChangesAsync();
+
+                await transaction.CommitAsync();
+
+                return rental;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
 
 		public IEnumerable<Rental> GetRentalsByCustomerName(string customerName)
 		{
             return _movieRentalDb.Rentals.Where(x=>x.CustomerName == customerName).ToList();
 		}
-
-        /*public string Payment(Rental rental)
-        {
-            switch(rental.PaymentMethod)
-			{
-				case "MbWay":
-					return PaymentProviders.MbWayProvider.;
-				case "PayPal":
-					return PaymentProviders.PayPalProvider;
-				default:
-					return null;
-			}
-        }*/
 
     }
 }
